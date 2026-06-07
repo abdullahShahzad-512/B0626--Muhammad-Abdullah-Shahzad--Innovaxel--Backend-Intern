bababc8e6d2e380340bda0474ac73fbcc00bcc96
Muhammad Abdullah Shahzad <bsef23a032@pucit.edu.pk>
2026-06-07 15:31:40 +0500

    feat: add models

---
+++ Models/Event.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Event
+{
+    [Key]
+    public int EventId {  get; set; }
+
+    [Required]
+    [Unique]
+    public  string Name{ get; set; }
+
+    [Range(1,int.MaxValue,ErrorMessage="Total Seats must be greater than zero")] 
+    public int TotalSeats{ get; set; }
+
+    public DateTime Date{ get;}
+    public DateTime CreatedAt { get; set; }
+
+    public void setDate(DateTime dt)
+    {
+        if (dt > DateTime.Now)
+            Date = dt;
+        else
+            throw Exception("Event must be in the future");
+    }
+}
---
+++ Models/Registration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Registration
+{
+    public string Username { get; set; }
+    [ForgeinKey]
+    public int EventID {  get; set; }
+    public DateTime RegisteredAt { get; set; }
+    public bool isCancelled { get; set; }=false;
+}
