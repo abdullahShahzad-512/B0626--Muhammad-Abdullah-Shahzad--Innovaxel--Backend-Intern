3c2c12ca17972aad8bc23f870e04aa1865487819
Muhammad Abdullah Shahzad <bsef23a032@pucit.edu.pk>
2026-06-07 19:10:23 +0500

    feat: add AppDbContext and register in Program.cs

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EventRegistrationAPI.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Registration> Registrations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Event>(entity =>
+            {
+                entity.HasIndex(e => e.Name).IsUnique();
+
+            });
+            modelBuilder.Entity<Registration>(entity =>
+            {
+                entity.HasOne(r => r.Event)
+                      .WithMany()
+                      .HasForeignKey(r => r.EventId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+        }
+    }
+}
